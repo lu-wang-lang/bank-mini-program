e2e16b2e54e0ed13b580bf64356cc092523e848b
wanglu <wanglu@able-elec.com>
2020-11-02 19:31:31 +0800

    引入图标字体

---
+++ bank-mini-program/pages/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="home-container">
+    123
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped lang="scss">
+.home-container {
+  width: 100vw;
+  height: 100vh;
+  background: rgba(240, 240, 242, 1);
+  padding: 10upx;
+  box-sizing: border-box;
+}
+</style>
--- bank-mini-program/pages/index/index.vue
+++ bank-mini-program/pages/index/index.vue
@@ -1,52 +1,51 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo"
+           src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{title}}</text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+export default {
+  data () {
+    return {
+      title: '这是一个银行测试系统'
+    }
+  },
+  onLoad () {
 
-		},
-		methods: {
+  },
+  methods: {
 
-		}
-	}
+  }
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
+  .text-area {
+    display: flex;
+    justify-content: center;
+    .title {
+      font-size: 36rpx;
+      color: #8f8f94;
+    }
+  }
+}
 </style>
